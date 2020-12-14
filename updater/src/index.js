669a8cc290dc02b886a488f9646cf634b9e46608
Jami Kousa <jami.kousa@helsinki.fi>
2020-12-14 18:30:46 +0200

    try catch

--- updater/src/index.js
+++ updater/src/index.js
@@ -6,7 +6,12 @@ import db from './db';
 
 const migrate = async () => {
   await waitForDatabaseConnection();
-  await db.raw("DELETE FROM \"knex_migrations_lock\" WHERE \"is_locked\" = 1")
+
+  try {
+    await db.raw("DELETE FROM \"knex_migrations_lock\" WHERE \"is_locked\" = 1")
+  } catch (err) {
+    logger.info('Could not delete migrations lock')
+  }
 
   await db.migrate.latest({
     directory: path.join(__dirname, '..', 'migrations'),
