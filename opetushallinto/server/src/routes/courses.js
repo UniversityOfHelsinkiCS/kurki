2628c341979d8655f5844005a4b9f18a3ca5d8fa
Kalle Ilves <kalleilv@gmail.com>
2021-01-13 13:17:17 +0200

    student transfer api

--- opetushallinto/server/src/routes/courses.js
+++ opetushallinto/server/src/routes/courses.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import getFrozenCourses from '../utils/getFrozenCourses';
 import getFrozenParticipantsByCourseId from '../utils/getFrozenParticipantsByCourseId';
+import Osallistuminen from '../models/Osallistuminen';
+import parseCourseId from '../utils/parseCourseId';
 
 const router = express.Router();
 
@@ -15,6 +17,25 @@ router.get('/:id/frozen-participants', async (req, res) => {
   const participants = await getFrozenParticipantsByCourseId(req.params.id);
 
   res.send(participants);
+});
+
+router.post('/:id/students-transferred', async (req, res) => {
+  const courseId = req.params.id;
+  const { code, term, year, type, number } = parseCourseId(courseId);
+
+  await Osallistuminen.query()
+    .where({
+      kurssikoodi: code,
+      lukuvuosi: year,
+      lukukausi: term,
+      tyyppi: type,
+      kurssiNro: number,
+    })
+    .patch({
+      siirto: 'F',
+    });
+
+  res.send({ courseId });
 });
 
 export default router;
