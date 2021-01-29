9773a3e95e45e7e6b4f3e56242a61cee21a01ce5
Kalle Ilves <kalleilv@gmail.com>
2021-01-29 15:21:53 +0200

    ignore ohtuprojekti in updater

--- updater/src/scripts/updateProgramme.js
+++ updater/src/scripts/updateProgramme.js
@@ -1,24 +1,33 @@
 import logger from '../utils/logger';
-import sisClient from '../utils/sisClient'
+import sisClient from '../utils/sisClient';
 import kurkiUpdater from '../utils/kurkiUpdater';
 import closeDatabaseConnection from '../utils/closeDatabaseConnection';
 
+const IGNORED_CODES = ['TKT20007'];
+
 const valid = (course) => {
-  return course.validityPeriod.endDate === '2021-08-01' || !course.validityPeriod.endDate
-} 
+  return (
+    course.validityPeriod.endDate === '2021-08-01' ||
+    !course.validityPeriod.endDate ||
+    !IGNORED_CODES.includes(course.code)
+  );
+};
 
 const main = async (code) => {
-  const courses = await sisClient.getCourseUnitsByProgramme(code)
-  const validCourses = courses.filter(valid)
-  const codes = validCourses.map(c => c.code); 
+  const courses = await sisClient.getCourseUnitsByProgramme(code);
+  const validCourses = courses.filter(valid);
+  const codes = validCourses.map((c) => c.code);
 
   await kurkiUpdater.updateCourseUnitsByCodes(codes);
   await kurkiUpdater.updateEnrolmentsByCodes(codes);
   await closeDatabaseConnection();
 };
 
-if (process.argv.length > 1 && ["500-K005", "500-M009","500-M010"].includes(process.argv[2]) ) {
+if (
+  process.argv.length > 1 &&
+  ['500-K005', '500-M009', '500-M010'].includes(process.argv[2])
+) {
   main(process.argv[2]).catch((error) => logger.error(error));
 } else {
-  logger.info("give unit code as parameter: 500-K005 500-M009 500-M010")
+  logger.info('give unit code as parameter: 500-K005 500-M009 500-M010');
 }
