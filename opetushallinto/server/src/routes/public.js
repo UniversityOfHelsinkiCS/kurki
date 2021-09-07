224570ed7f920da68e11b7c36c8e5468d21661b3
Kalle Ilves <kalleilv@gmail.com>
2021-09-07 16:28:25 +0300

    labtool course by id endpoint

--- opetushallinto/server/src/routes/public.js
+++ opetushallinto/server/src/routes/public.js
@@ -1,6 +1,8 @@
 import express from 'express';
 
 import getLabtoolCourses from '../utils/labtool/getLabtoolCourses';
+import parseCourseId from '../utils/parseCourseId';
+import Kurssi from '../models/Kurssi';
 import { UserInputError } from '../errors';
 
 const router = express.Router();
@@ -21,6 +23,29 @@ router.get('/labtool/courses', async (req, res) => {
   const courses = await getLabtoolCourses({ year: parsedYear, term });
 
   res.send(courses);
+});
+
+router.get('/labtool/courses/:id', async (req, res) => {
+  const { id } = req.params;
+  const { code, term, year, type, number } = parseCourseId(id);
+
+  const kurssi = await Kurssi.query()
+    .findOne({
+      kurssikoodi: code,
+      lukuvuosi: year,
+      lukukausi: term,
+      tyyppi: type,
+      kurssiNro: number,
+    })
+    .withGraphFetched('[opetustehtavanHoidot.henkilo]');
+
+  const teachers = kurssi.opetustehtavanHoidot
+    .map((oh) => oh.henkilo?.ktunnus)
+    .filter(Boolean);
+
+  res.send({
+    teachers,
+  });
 });
 
 export default router;
